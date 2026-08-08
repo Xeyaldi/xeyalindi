9759c7e10fdcdd128b5f36800c226c97b48e5d0d
Xeyaldi <167771989+Xeyaldi@users.noreply.github.com>
2026-08-08 14:45:39 +0400

    Auto-upload by bot: xeyalindi/xeyal/plugins/skip.py

---
+++ xeyalindi/xeyal/plugins/skip.py
@@ -0,0 +1,17 @@
+
+
+from pyrogram import filters, types
+
+from xeyal import anon, app, db, lang
+from xeyal.helpers import can_manage_vc
+
+
+@app.on_message(filters.command(["skip", "next", "atla"]) & filters.group & ~app.bl_users)
+@lang.language()
+@can_manage_vc
+async def _skip(_, m: types.Message):
+    if not await db.get_call(m.chat.id):
+        return await m.reply_text(m.lang["not_playing"])
+
+    await anon.play_next(m.chat.id)
+    await m.reply_text(m.lang["play_skipped"].format(m.from_user.mention))
